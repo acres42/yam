fb479b671b8bbf99aeadca726cdceaa8095f9fbe
AC Roselee <acroselee@gmail.com>
2025-06-29 20:25:09 -0400

    First draft for prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite'
-
-// https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	trailingSlash: 'never',
-	redirects: {
-		'/about': 'https://${SITE_BASE_DOMAIN}/about',
-		'/news': {
-			status: 302,
-			destination: 'https://${SITE_BASE_DOMAIN}/news',
-		},
-	},
-})
+  integrations: [tailwind()],
+});
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+        tertiary: "var(--color-tertiary)",
+        bg: "var(--color-bg)",
+        text: "var(--color-text)"
+      },
+    },
+  },
+  plugins: [],
+}
