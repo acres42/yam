98dac77c071a4a8a7b6051884955572bcef03147
AC Roselee <acroselee@gmail.com>
2025-07-04 20:21:30 -0400

    Edits to header, phase 1, per CJR

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,29 @@
-import tailwindConfig from 'stylelint-config-tailwindcss';
-import standardConfig from 'stylelint-config-standard';
-
+// stylelint.config.js
 export default {
-  extends: [standardConfig, tailwindConfig],
+  customSyntax: 'postcss-scss',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-tailwindcss',
+  ],
   rules: {
-    'at-rule-no-deprecated': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+          // optionally add custom at-rules here
+        ],
+      },
+    ],
+    'at-rule-no-deprecated': null, // to silence @apply warning if desired
   },
+  ignoreFiles: ['**/node_modules/**', '**/dist/**'],
 };
