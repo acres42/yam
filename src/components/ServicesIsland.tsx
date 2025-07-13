5d5a2f6bf70ede111ad17c7433c42885b570eca0
AC Roselee <acroselee@gmail.com>
2025-07-13 14:21:28 -0400

    Touchups to typescript ServicesIsland

--- src/components/ServicesIsland.tsx
+++ src/components/ServicesIsland.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from "preact/hooks";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { servicesList } from "@/types/content";
+import servicesList from "@/types/content";
 import StethoscopeIcon from "./StethoscopeIcon.tsx";
 export default function ServicesIsland() {
   useEffect(() => {
@@ -30,7 +30,7 @@ export default function ServicesIsland() {
       </h2>
 
       <div class="mx-auto grid w-fit grid-cols-1 items-stretch gap-x-20 gap-y-4 text-left sm:grid-cols-2">
-        <ul class="space-y-6 h-full">
+        <ul class="h-full space-y-6">
           {firstColumn.map((service, index) => (
             <li
               key={index}
@@ -44,7 +44,7 @@ export default function ServicesIsland() {
           ))}
         </ul>
 
-        <ul class="space-y-6 h-full">
+        <ul class="h-full space-y-6">
           {secondColumn.map((service, index) => (
             <li
               key={index + firstColumn.length}
--- src/types/content.ts
+++ src/types/content.ts
@@ -1,22 +1,25 @@
-export const services = [
-  "Fevers, allergies, sore throat, and pinkeye",
-  "Well Child Checks age 5+",
-  "Vomiting, Diarrhea, Constipation",
-  "Reproductive Health counseling including Medication",
-  "ADHD evaluation and Medication management",
-  "School Stress and Peer challenges",
-  "Transition from Middle School to High School OR High School to College",
-  "Referral to Specialists when appropriate",
-  "Skin rashes, dry skin, itching, eczema and acne",
-  "Vaccines available via mobile service",
-  "Camp and Sports Physicals when telehealth appropriate",
-  "Growth and Weight Optimization including medication",
-  "In-depth Mental Health Services 12yr and older",
-  "Nutrition and Sleep Support",
-  "Follow up after Emergency Department visit",
-];
+const TelehealthService = {
+  Fevers: "Fevers, allergies, sore throat, and pinkeye",
+  WellChecks: "Well Child Checks age 5+",
+  Vomiting: "Vomiting, Diarrhea, Constipation",
+  ReproHealth: "Reproductive Health counseling including Medication",
+  ADHD: "ADHD evaluation and Medication management",
+  SchoolStress: "School Stress and Peer challenges",
+  Transitions:
+    "Transition from Middle School to High School OR High School to College",
+  SpecialistReferral: "Referral to Specialists when appropriate",
+  Rashes: "Skin rashes, dry skin, itching, eczema and acne",
+  Vaccines: "Vaccines available via mobile service",
+  Physicals: "Camp and Sports Physicals when telehealth appropriate",
+  Growth: "Growth and Weight Optimization including medication",
+  MentalHealth: "In-depth Mental Health Services 12yr and older",
+  Nutrition: "Nutrition and Sleep Support",
+  EmergencyFollowup: "Follow up after Emergency Department visit",
+} as const;
 
-export const servicesList: string[] = Object.values(services);
+const servicesList: string[] = Object.values(TelehealthService);
+
+export default servicesList;
 
 export type ContactInfo = {
   name: string;
