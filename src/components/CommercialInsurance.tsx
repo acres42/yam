cfe7781a67597a92e2087e94a3f367075ca63d8d
AC Roselee <acroselee@gmail.com>
2025-10-23 13:49:39 -0700

    Add sizing changes to Hero and comm ins component for smaller screens

--- src/components/CommercialInsurance.tsx
+++ src/components/CommercialInsurance.tsx
@@ -2,10 +2,10 @@ import { commercialInsurancesAccepted } from "@/types/content";
 
 export const CommercialInsurance = () => (
   <section className="mx-auto rounded border border-darkblue bg-yellow-200 p-4 px-4 py-8 text-center text-gray-800 shadow-sm sm:px-6 sm:py-10 md:px-8 lg:px-16">
-    <h2 className="mb-6 text-centert text-darkblue text-3xl font-semibold">
+    <h2 className="mb-6 text-center text-darkblue text-2xl font-semibold md:text-3xl lg:text-4xl">
       Commercial Insurances Accepted
     </h2>
-    <ul className="mx-auto flex max-w-5xl flex-wrap justify-center gap-x-8 gap-y-4 text-sm text-darkblue sm:text-base">
+    <ul className="mx-auto flex max-w-5xl flex-wrap justify-center gap-x-8 gap-y-4 text-darkblue md:text-xs lg:text-sm">
       {commercialInsurancesAccepted.map((insurance) => (
         <li
           key={insurance}
