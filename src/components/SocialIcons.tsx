1b91d2c7efd60c5f62266ca5639f109d048d2ea8
AC Roselee <acroselee@gmail.com>
2025-10-23 14:22:29 -0700

    Add padding to Follow me in footer

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -5,7 +5,7 @@ export default function SocialIcons() {
   return (
     <div class="bg-tertiary py-8 text-bg">
       <div class="flex items-center justify-center gap-6 text-2xl">
-        <p class="text-xl text-secondary">
+        <p class="px-2 text-xl text-secondary">
           Follow me at{" "}
           <span class="text-2xl font-bold text-bg">askNPCarey</span>
         </p>
